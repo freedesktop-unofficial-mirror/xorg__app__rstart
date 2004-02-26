3283d8fafa02c9e93a71ad83f2f3788c9cb54e2a
Egbert Eich <eich@suse.de>
2004-02-26 13:36:15 +0000

    readding XFree86's cvs IDs

--- auth.c
+++ auth.c
@@ -23,7 +23,7 @@
 /* OF WHETHER IN AN ACTION IN CONTRACT, TORT OR NEGLIGENCE, ARISING OUT	*/
 /* OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.	*/
 /************************************************************************/
-/* $XFree86$ */
+/* $XFree86: xc/programs/rstart/auth.c,v 1.5 2001/07/25 15:05:15 dawes Exp $ */
 
 #include <stdio.h>
 #include <X11/Xos.h>
